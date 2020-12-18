a84b01a3fc6fec93a3788837a32ec0a1c65cb545
Constantin Gabriel <39054700+Kira060200@users.noreply.github.com>
2020-12-18 19:19:24 +0200

    Changed some header arguments

--- mtx_header.h
+++ mtx_header.h
@@ -3,9 +3,9 @@
 #include<pthread.h>
 
 int mtxopen(pid_t pid);
-int mtxclose(int id, pid_t pid);
+int mtxclose(int id);
 int mtxlock(int id, pid_t pid);
-int mtxunlock(int id, pid_t pid);
+int mtxunlock(int id);
 int mtxlist(pid_t *list, int nlist);
-int mtxgrant(int id, pid_t gr);
+int mtxgrant(int id);
 #endif
