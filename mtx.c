14f7f8019808d57307d3e6a17290573c964999b4
Constantin Gabriel <39054700+Kira060200@users.noreply.github.com>
2020-12-18 19:18:20 +0200

    Implemented mtxclose, mtxlock, mtxunlock

--- mtx.c
+++ mtx.c
@@ -7,20 +7,23 @@
 #include "mtx_header.h"
 
 bool created_list;
-int nlist = 0;
+int nr_mlist = 0;
+int nr_plist = 0;
+pid_t wake_process;
 struct process
 {
 	pid_t id;
 	bool status; //1 for waiting, else 0
 	int * h_mlist;
-	int h_mlist_size ;
+	int h_mlist_size;
 };
 struct mutex
 {
 	int id;
 	bool opened;
 	bool locked;
-	//pid_t * plist;
+	pid_t * wlist;
+	int nr_wlist;
 };
 struct process * plist;
 struct mutex * mlist;
@@ -32,8 +35,8 @@ int mtxopen(pid_t pid)
 		mlist = malloc(sizeof(struct mutex));
 		mlist[0].locked = false;
 		mlist[0].opened = false;
-		mlist[0].id = nlist;
-		nlist = 1;
+		mlist[0].id = nr_mlist;
+		nr_mlist = 1;
 	}
 	// else
 	// {
@@ -44,10 +47,11 @@ int mtxopen(pid_t pid)
 	p->id = pid;
 	p->status = false;
 	bool available = false;
-	for (int i = 0; i<nlist; i++)
+	for (int i = 0; i<nr_mlist; i++)
 	{
 		if(!mlist[i].opened)
 		{
+			// TO DO: daca trece de 2 ori prin acelasi mutex il trece de 2 ori :(
 			p->h_mlist = realloc(p->h_mlist, sizeof(int*)*(p->h_mlist_size + 1));
 			p->h_mlist[p->h_mlist_size] = mlist[i].id;
 			p->h_mlist_size ++;
@@ -59,14 +63,87 @@ int mtxopen(pid_t pid)
 	}
 	if(!available)
 	{
-		mlist = realloc(mlist, sizeof(struct mutex)*(nlist+1));
-		mlist[nlist].opened = true;
-		mlist[nlist].locked = false;
-		mlist[nlist].id = nlist;
+		mlist = realloc(mlist, sizeof(struct mutex)*(nr_mlist+1));
+		mlist[nr_mlist].opened = true;
+		mlist[nr_mlist].locked = false;
+		mlist[nr_mlist].id = nr_mlist;
 		p->h_mlist = realloc(p->h_mlist, sizeof(int*)*(p->h_mlist_size + 1));
-		p->h_mlist[p->h_mlist_size] = mlist[nlist].id;
+		p->h_mlist[p->h_mlist_size] = mlist[nr_mlist].id;
 		p->h_mlist_size ++;
-		nlist++;
+		nr_mlist++;
 	}
 	return 0;
+}
+int mtxclose(int id)
+{
+	if (created_list)
+	{
+		for (int i = 0; i<nr_mlist; i++)
+		{
+			if(mlist[i].id == id)
+			{
+				mlist[i].opened = false;
+				// for(int j=i; j<nlist-1; j++)
+				// 	mlist[j]=mlist[j+1];
+				// mlist = realloc(mlist, sizeof(struct mutex)*(--nlist));
+				return 0;
+			}
+		}
+	}
+	return -1;
+}
+int mtxlock(int id, pid_t pid)
+{
+	int mutex_found = -1;
+	int process_found = -1;
+	if(created_list)
+	{
+		for(int i=0; i<nr_mlist; i++)
+		{
+			if(mlist[i].id == id && mlist[i].opened == true)
+			{
+				mutex_found = i;
+				break;
+			}
+		}
+		if(mutex_found == -1)
+			return -1;
+		// nr_plist = sizeof(&plist) / sizeof(struct process);
+		for(int i=0; i<nr_plist; i++)
+			if(plist[i].id == pid)
+			{
+				process_found = i;
+				break;
+			}
+		if(process_found == -1)
+			return -1;
+		plist[process_found].status = true;
+		if(mlist[mutex_found].locked == false)
+		{
+			mlist[mutex_found].locked = true;
+			return 0;
+		}
+		mlist[mutex_found].wlist = realloc(mlist[mutex_found].wlist, sizeof(pid_t)*(mlist[mutex_found].nr_wlist++));
+		mlist[mutex_found].wlist[mlist[mutex_found].nr_wlist-1] = pid;
+		while(wake_process != pid);
+		plist[process_found].status = false;
+		return 0;
+	}
+	return -1;
+}
+int mtxunlock(int id)
+{
+	if(created_list)
+	{
+		for(int i=0; i<nr_mlist; i++)
+		{
+			if(mlist[i].id == id)
+			{
+				mlist[i].locked = false;
+				mtxgrant(id);
+				return 0;
+			}
+		}
+	}
+	return -1;
 }
