e24d10deb79a4a3dcfe54bc13bf512698051a829
Constantin Gabriel <39054700+Kira060200@users.noreply.github.com>
2021-01-05 17:38:05 +0200

    Add files via upload

--- mtx_header.h
+++ mtx_header.h
@@ -1,11 +1,19 @@
 #ifndef MTX_FUNCTIONS
 #define MTX_FUNCTIONS
 #include<pthread.h>
-
-int mtxopen(pid_t pid);
+#include<stdbool.h>
+struct mutex
+{
+	int id;
+	bool opened;
+	bool locked;
+	pid_t * wlist;
+	int nr_wlist;
+};
+int mtxopen(pid_t pid, int id);
 int mtxclose(int id);
 int mtxlock(int id, pid_t pid);
 int mtxunlock(int id);
-int mtxlist(pid_t *list, int nlist);
+int mtxlist(int *v);
 int mtxgrant(int id);
 #endif
